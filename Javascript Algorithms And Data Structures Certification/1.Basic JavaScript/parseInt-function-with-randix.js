00a0c45d70b1b20d5964c3825895298f59de246c
GeorgeGR <GeorgeGR>
2018-12-20 14:19:59 +0200

    Basic JS competed

---
+++ Javascript Algorithms And Data Structures Certification/1.Basic JavaScript/parseInt-function-with-randix.js
@@ -0,0 +1,9 @@
+/**
+ * parseInt function: https://devdocs.io/javascript/global_objects/parseint
+ * @param str: a given string, randix: a number for converting to 2,8, 10, 16 system.
+ *  */
+function convertToInteger(str) {
+    return parseInt(str, 2);
+  }
+  
+  convertToInteger("10011");
