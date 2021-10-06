63873ca344b650bbaeb32a4ec334c3c609d6273c
glgeorgiou <glgeorgiou@yahoo.gr>
2021-10-06 14:59:31 +0300

    Finally js control my eliminating functionality

--- custom-work/age-control/check-age.js
+++ custom-work/age-control/check-age.js
@@ -1,14 +1,6 @@
 let btnValidate = document.querySelector("#btnValidate");
 btnValidate.addEventListener("click",validate)
 
-/*Calculation function.
-@parameter: date dd-mm-yyyy
-@Return: an integer number*/
-function calculateAge(dob) {
-    const ageInMillisecongs = Date.now() - new Date(dob);
-    return Math.floor(ageInMillisecongs / (1000 * 60 * 60 * 24 * 365.25));
-  }
-
 
   /*Validate function.
   @Parameter: event */
@@ -18,24 +10,26 @@ function validate(evt) {
     evt.preventDefault();
 
     //Take the values
-    let mera = document.querySelector("#mera").selectedIndex;
-    let minas = document.querySelector("#minas").selectedIndex;
+    let mera = document.querySelector("#mera");
+    let minas = document.querySelector("#minas");
     let etos = document.querySelector("#etos");
-    let etosValue = etos.options[etos.selectedIndex].text;
     let showAge = document.querySelector("#output");
-    let age='';   
+    let nMera = mera.value;
+    let nMinas = minas.value;
+    let nEtos = Number(etos.value);
+    let age='', ageTmp = '';   
 
-    date = etosValue+"-"+minas+"-"+mera;
+    
+    //Calculate age
+    etosTmp = new Date()
+    numEtos = Number(etosTmp.getFullYear())
+    age =  numEtos-nEtos;
 
-    //Calculate and show age
-    age = calculateAge(date);
-
-    if (age != 'NaN') {
-      showAge.value = age;
-    } else {
-      showAge.value = 'Sorry, invalid age';
-    }
-
-    //TMP
-    console.log('The date is: '+date+' - Your age is '+age+'.');
+    //Check age
+  if (age <= 21) {
+    showAge.value = 'Sorry, you \'re less than 21.'
+  } else {
+    showAge.value = age;
+  }
+    //NOTE: change color and text of the button in any case
 }
