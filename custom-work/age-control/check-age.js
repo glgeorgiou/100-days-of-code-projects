3b4e9b1c1b78b351242b5b13ffa1878a9d29ce63
glgeorgiou <glgeorgiou@yahoo.gr>
2021-10-11 17:14:41 +0300

    designing more value controls in age control project

--- custom-work/age-control/check-age.js
+++ custom-work/age-control/check-age.js
@@ -26,11 +26,17 @@ function validate(evt) {
 
     //Check age
     if (nMera =='0' || nMinas=='0' || nEtos==0) {
-      btnValidate.innerText = 'Invalid values. Try again'
+      btnValidate.innerText = 'One or more zero values. Try again'
       btnValidate.removeAttribute("style");
       showAge.value = '';
     } else {
       if (age <= 21) {
+        /**
+         * More validatations:
+         * 1. dis year
+         * 2. valid end date accordng to month
+         * 3. compare also month and date in age in order to control < 21 years old
+         */
         btnValidate.innerText = 'Sorry, you \'re less than 21.'
         showAge.value = age;
         btnValidate.setAttribute("style","background-color: red")
@@ -41,3 +47,10 @@ function validate(evt) {
       }
     }
 }
+
+/**
+ * TEMP
+ * mera.addEventListener('change', (evt) => {
+console.log('The day is '+evt.target.value+', and is type of '+typeof Number(evt.target.value));
+});
+ */
